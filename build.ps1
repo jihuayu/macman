6c87fc135333051e85e2bc561b69e587e656a588
Summpot <Summpot@outlook.com>
2020-02-05 21:27:03 +0800

    update

--- build.ps1
+++ build.ps1
@@ -1,3 +1,5 @@
-dotnet publish .\src\macman.PSModule\macman.PSModule.csproj -c release -o $PWD/dist 
-Copy-Item Macman.psd1 dist -Force
-Import-Module ./dist/Macman.psd1
+Remove-Item -Recurse ./dist
+mkdir .\dist
+dotnet publish .\src\macman.PSModule\macman.PSModule.csproj -c release -o .\dist --self-contained
+Copy-Item Macman.psd1 .\dist -Force
+Import-Module .\dist\Macman.psd1
